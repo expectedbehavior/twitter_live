0b314a66cd0d50bb44d5f27b7b87d9bbfd3f1445
Matt Gordon <matt@expectedbehavior.com>
2009-12-03 23:26:24 -0500

    add user factory

--- test/factories.rb
+++ test/factories.rb
@@ -1,1 +1,10 @@
- 
+Factory.define(:user) do |u|
+  u.sequence(:email) { |n| "email#{n}@example.com" }
+  u.sequence(:login) { |n| "tweeterer#{n}" }
+  u.name { "John Doe" }
+  u.password "password"
+  u.password_confirmation "password"
+  u.activated_at 1.day.ago
+  u.state "active"
+end
+
