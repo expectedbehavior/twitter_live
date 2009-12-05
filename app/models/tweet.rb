ececc042d3b307e81f300a428d5b8a9032181b14
Matt Gordon <matt@expectedbehavior.com>
2009-12-05 09:15:28 -0500

    super basic trends method

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,4 +6,35 @@ class Tweet < ActiveRecord::Base
     
     set_property :delta => true
   end
+
+  def self.trends(options = {})
+    require 'ruby-debug'
+    Debugger.start
+    debugger
+
+    default_options = { 
+      :start_time => 1.minute.ago
+    }
+    options = options.dup
+    options.reverse_merge!(default_options)
+    
+    trending_tweets = Tweet.find(:all, :conditions => {:created_at => options[:start_time]..1.day.from_now})
+    
+
+    
+    trend_count = {}
+    trending_tweets.each do |tweet|
+      tags = tweet.content.split(" ").select {|word| word =~ /^#/ }
+      tags.each do |t|
+        trend_count[t] ||= 0
+        trend_count[t] +=  1
+      end
+    end
+    
+    sorted_trend_count = trend_count.sort {|a,b| b[1] <=> a[1] }
+    sorted_trend_count[0..9].map {|x| x[0] }
+    
+    
+  end
+
 end
